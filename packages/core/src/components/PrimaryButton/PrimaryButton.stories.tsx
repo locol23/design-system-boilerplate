69cc4e726d4d020f6be65284923289426e159618
Yoshitaka Terazawa <locol6622@gmail.com>
2023-03-19 22:57:21 +0900

    chore: Use Storybook v7 RC

--- packages/core/src/components/PrimaryButton/PrimaryButton.stories.tsx
+++ packages/core/src/components/PrimaryButton/PrimaryButton.stories.tsx
@@ -1,17 +1,20 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 import { typography } from '../../styles'
 import { PrimaryButton } from './PrimaryButton'
 
-type Story = ComponentStoryObj<typeof PrimaryButton>
+const meta = {
+  title: 'PrimaryButton',
+  component: PrimaryButton,
+} satisfies Meta<typeof PrimaryButton>
 
-export default {
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
   args: {
     fontSize: typography.size.m,
     text: 'Primary Button',
     onClick: () => alert('PrimaryButton'),
   },
-  component: PrimaryButton,
-  title: 'PrimaryButton',
-} as ComponentMeta<typeof PrimaryButton>
-
-export const Default = {} as Story
+}
